842f662bbb94b208075774dd782a22bd8e97cff8
dragan <draganandonovski995@gmail.com>
2017-08-19 23:01:22 +0200

    WBS Initial

---
+++ src/main/java/com/wbs/WbsApplication.java
@@ -0,0 +1,12 @@
+package com.wbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WbsApplication.class, args);
+	}
+}
