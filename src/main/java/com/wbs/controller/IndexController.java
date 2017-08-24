da14321aafd080ed4a0e305e714827852dbceeac
ljubomirsk <ljubomir_ignov@hotmail.com>
2017-08-24 15:24:55 +0200

    Added querying dbpedia endpoint.

--- src/main/java/com/wbs/controller/IndexController.java
+++ src/main/java/com/wbs/controller/IndexController.java
@@ -1,5 +1,12 @@
 package com.wbs.controller;
 
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.vocabulary.RDF;
+import org.apache.jena.vocabulary.RDFS;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +23,17 @@ public class IndexController {
     public String getCity(@RequestBody String city) {
 
         System.out.println(city);
+
+        String endpoint = "http://dbpedia.org/sparql";
+
+        String queryString = "select distinct ?Concept where {[] a ?Concept} LIMIT 100";;
+        Query query = QueryFactory.create(queryString);
+        QueryExecution exec = QueryExecutionFactory.sparqlService(endpoint, query);
+        ResultSet resSet = exec.execSelect();
+        while (resSet.hasNext()){
+            QuerySolution solution = resSet.nextSolution();
+            System.out.println(solution.get("Concept"));
+        }
 
         return "index";
     }
