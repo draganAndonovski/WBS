012b03c7e68f3b6d527c8d578e9d1d7623f9059c
JArsov <orce.arsov@yahoo.com>
2017-08-25 19:12:22 +0200

    Add basic processing for all capitals

--- src/main/java/com/wbs/controller/IndexController.java
+++ src/main/java/com/wbs/controller/IndexController.java
@@ -1,10 +1,7 @@
 package com.wbs.controller;
 
 import org.apache.jena.query.*;
-import org.apache.jena.rdf.model.Model;
-import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.rdf.model.Property;
-import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.*;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
 import org.springframework.stereotype.Controller;
@@ -14,26 +11,87 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class IndexController {
 
+    static String startingUrl = "http://dbpedia.org/sparql";
+
+    static String resourceUrl = "http://dbpedia.org/resource/";
+
+    static String dataUrl = "http://dbpedia.org/data/";
+
+    static String dboPrefix = "http://dbpedia.org/ontology/";
+
+    static String dbpPrefix = "http://dbpedia.org/property/";
+
     @RequestMapping(value = "/")
     public String index() {
         return "index";
     }
 
+    private ResultSet getResults(Query query) {
+        return QueryExecutionFactory.sparqlService(startingUrl, query).execSelect();
+    }
+
+    private ResultSet getOtherCitiesResults() {
+        ParameterizedSparqlString qs = new ParameterizedSparqlString("" +
+                "prefix dbo:    <http://dbpedia.org/ontology/>\n" +
+                "prefix dbp:    <http://dbpedia.org/property/>\n" +
+                "prefix foaf:    <http://xmlns.com/foaf/0.1/>\n" +
+                "prefix dct:    <http://purl.org/dc/terms/>\n" +
+                "prefix rdf:    <http://www.w3.org/1999/02/22-rdf-syntax-ns#>\n" +
+                "prefix rdfs:    <http://www.w3.org/2000/01/rdf-schema#>\n" +
+                "\n" +
+                "select ?capital where {\n" +
+                "  ?country rdf:type dbo:Country .\n" + // get all countries
+                "  ?country dct:subject ?unitedNations .\n" + // that are in the United Nations
+                "  ?country dbo:capital ?capital .\n" + // with capital city
+                "  ?capital dbo:populationTotal ?nesto .\n" + // that has populationTotal property
+                "}"
+        );
+
+        Model model = ModelFactory.createDefaultModel();
+        model.read(dataUrl + "Category:Member_states_of_the_United_Nations", "TURTLE");
+        Resource unitedNationsResource = model.getResource(resourceUrl + "Category:Member_states_of_the_United_Nations");
+        qs.setParam("unitedNations", unitedNationsResource); // set the united nations as parameter to the query
+
+
+        return getResults(QueryFactory.create(qs.asQuery()));
+    }
+
     @RequestMapping(value = "/city", method = RequestMethod.POST)
     public String getCity(@RequestBody String city) {
+        int count = 0;
 
-        System.out.println(city);
+        //System.out.println(city);
 
-        String endpoint = "http://dbpedia.org/sparql";
+        // read the model
+        Model cityModel = ModelFactory.createDefaultModel();
+        cityModel.read(dataUrl + city, "TURTLE");
+        Resource cityResource = cityModel.getResource(resourceUrl + city);
 
-        String queryString = "select distinct ?Concept where {[] a ?Concept} LIMIT 100";;
-        Query query = QueryFactory.create(queryString);
-        QueryExecution exec = QueryExecutionFactory.sparqlService(endpoint, query);
-        ResultSet resSet = exec.execSelect();
-        while (resSet.hasNext()){
-            QuerySolution solution = resSet.nextSolution();
-            System.out.println(solution.get("Concept"));
+        // System.out.println(cityResource);
+
+        // create property for population (dbo:populationTotal)
+        Property populationProperty = ResourceFactory.createProperty(dboPrefix + "populationTotal");
+        int cityPopulation = Integer.parseInt(cityResource.getProperty(populationProperty).getString());
+
+        //System.out.println(cityPopulation);
+
+        // get info about all the country capitals
+        ResultSet resSet = getOtherCitiesResults();
+
+        // iterate the set and print the number of inhabitants in each capital
+        while(resSet.hasNext()) {
+            Resource resource = resSet.next().get("capital").asResource();
+            String otherCityUrl = resource.toString().replaceAll("resource", "data");
+            Model otherCityModel = ModelFactory.createDefaultModel();
+            otherCityModel.read(otherCityUrl,"TURTLE");
+            resource = otherCityModel.getResource(resource.toString());
+            int otherCityPopulation = Integer.parseInt(resource.getProperty(populationProperty).getString());
+            System.out.println(resource + " has " + otherCityPopulation + " inhabitants");
+            ++count;
         }
+
+        // print total number of cities processed (167)
+        //System.out.println("VKUPNO " + count);
 
         return "index";
     }
