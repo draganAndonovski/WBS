79ec5ae24c47380f186708674193a89aa23b1b96
ljubomirsk <ljubomir_ignov@hotmail.com>
2017-08-29 13:34:42 +0200

    Added filtering cities by population and area.

--- src/main/java/com/wbs/controller/IndexController.java
+++ src/main/java/com/wbs/controller/IndexController.java
@@ -2,10 +2,11 @@ package com.wbs.controller;
 
 import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
-import org.apache.jena.vocabulary.RDF;
-import org.apache.jena.vocabulary.RDFS;
+import org.apache.jena.sparql.vocabulary.FOAF;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RequestMapping(value = "/")
 @Controller
@@ -43,7 +44,7 @@ public class IndexController {
                 "  ?country rdf:type dbo:Country .\n" + // get all countries
                 "  ?country dct:subject ?unitedNations .\n" + // that are in the United Nations
                 "  ?country dbo:capital ?capital .\n" + // with capital city
-                "  ?capital dbo:populationTotal ?nesto .\n" + // that has populationTotal property
+                "  ?capital dbo:populationTotal ?population .\n" + // that has populationTotal property
                 "}"
         );
 
@@ -63,6 +64,7 @@ public class IndexController {
         //System.out.println(city);
 
         // read the model
+        city = city.replaceAll(" ", "_");
         Model cityModel = ModelFactory.createDefaultModel();
         cityModel.read(dataUrl + city, "TURTLE");
         Resource cityResource = cityModel.getResource(resourceUrl + city);
@@ -71,23 +73,55 @@ public class IndexController {
 
         // create property for population (dbo:populationTotal)
         Property populationProperty = ResourceFactory.createProperty(dboPrefix + "populationTotal");
+        Property areaProperty = ResourceFactory.createProperty(dboPrefix + "PopulatedPlace/areaTotal");
         int cityPopulation = Integer.parseInt(cityResource.getProperty(populationProperty).getString());
+        float cityArea = Float.parseFloat(cityResource.getProperty(areaProperty).getString());
 
+//        System.out.println(cityArea);
         //System.out.println(cityPopulation);
 
         // get info about all the country capitals
         ResultSet resSet = getOtherCitiesResults();
 
+        ArrayList<String[]> citiesPopulations = new ArrayList<>();
+
         // iterate the set and print the number of inhabitants in each capital
-        while(resSet.hasNext()) {
+        while (resSet.hasNext()) {
             Resource resource = resSet.next().get("capital").asResource();
             String otherCityUrl = resource.toString().replaceAll("resource", "data");
             Model otherCityModel = ModelFactory.createDefaultModel();
-            otherCityModel.read(otherCityUrl,"TURTLE");
+            otherCityModel.read(otherCityUrl, "TURTLE");
             resource = otherCityModel.getResource(resource.toString());
-            int otherCityPopulation = Integer.parseInt(resource.getProperty(populationProperty).getString());
-            System.out.println(resource + " has " + otherCityPopulation + " inhabitants");
+            int otherCityPopulation;
+            float otherCityArea;
+            try {
+                otherCityPopulation = Integer.parseInt(resource.getProperty(populationProperty).getString());
+                otherCityArea = Float.parseFloat(resource.getProperty(areaProperty).getString());
+                System.out.println(otherCityArea);
+                String cityName = resource.getProperty(FOAF.name).getString();
+                String[] cityAttributes = new String[3];
+                cityAttributes[0] = cityName;
+                cityAttributes[1] = Integer.toString(otherCityPopulation);
+                cityAttributes[2] = Float.toString(otherCityArea);
+                citiesPopulations.add(cityAttributes);
+            } catch (Exception e) {
+                continue;
+            }
+            System.out.println(resource + " has " + otherCityPopulation + " inhabitants and Area: " + otherCityArea);
             ++count;
+        }
+
+        // filter cities with population and area bigger or smaller by 30% of the chosen city
+        for (String[] cityAttr : citiesPopulations) {
+            int diffPopulation = cityPopulation * 30 / 100;
+            float diffArea = cityArea * 30 / 100;
+            if (Integer.parseInt(cityAttr[1]) + diffPopulation < cityPopulation || Integer.parseInt(cityAttr[1]) - diffPopulation > cityPopulation) {
+                continue;
+            }
+            if (Float.parseFloat(cityAttr[2]) + diffArea < cityArea || Float.parseFloat(cityAttr[2]) - diffArea > cityArea) {
+                continue;
+            }
+            System.out.println(cityAttr[0] + " " + cityAttr[1] + " " + cityAttr[2]);
         }
 
         // print total number of cities processed (167)
